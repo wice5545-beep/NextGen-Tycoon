5bb48604bd8f0d717e4348180a27f37b36c3a710
Netral Build <netral@build.dev>
2026-06-05 20:11:24 +0200

    Force paysage (landscape) - comme Console Tycoon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,7 @@ const config: CapacitorConfig = {
   backgroundColor: "#070a12",
   android: {
     allowMixedContent: false,
+    orientation: 'landscape',
   },
   server: {
     androidScheme: "https",
